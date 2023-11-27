9c5bc690e8e2f5377e3ce59bac7ccd56ea7550d7
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2023-11-27 16:49:55 -0300

    Finished the Stack, Certifications, Project and Contact sections of the first draft of the website. Added mobile responsiveness

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const clickableContents = document.querySelectorAll(".clickable-content");
+  const modals = document.querySelectorAll(".modal");
+  const closeButtons = document.querySelectorAll(".close");
+
+  clickableContents.forEach((clickable, index) => {
+    clickable.onclick = function () {
+      modals[index].style.display = "block";
+    };
+  });
+
+  closeButtons.forEach((button, index) => {
+    button.onclick = function () {
+      modals[index].style.display = "none";
+    };
+  });
+
+  window.onclick = function (event) {
+    modals.forEach((modal) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  };
+});
