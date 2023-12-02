52b91dd811c5cf7d90405a80cae4929dd7fd8de7
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2023-12-01 23:22:01 -0300

    Added transitions to modal element

--- website/js/script.js
+++ website/js/script.js
@@ -33,19 +33,33 @@ document.addEventListener("DOMContentLoaded", () => {
   clickableContents.forEach((clickable, index) => {
     clickable.onclick = function () {
       modals[index].style.display = "block";
+      setTimeout(() => {
+        modals[index].style.opacity = 1;
+        modals[index].querySelector(".modal-content").style.transform =
+          "scale(1)";
+      }, 10);
     };
   });
 
   closeButtons.forEach((button, index) => {
     button.onclick = function () {
-      modals[index].style.display = "none";
+      modals[index].style.opacity = 0;
+      modals[index].querySelector(".modal-content").style.transform =
+        "scale(0.95)";
+      setTimeout(() => {
+        modals[index].style.display = "none";
+      }, 200);
     };
   });
 
   window.onclick = function (event) {
     modals.forEach((modal) => {
       if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.opacity = 0;
+        modal.querySelector(".modal-content").style.transform = "scale(0.95)";
+        setTimeout(() => {
+          modal.style.display = "none";
+        }, 500);
       }
     });
   };
